d9c452afa8ee555689d2b15168fc6b36195c9e57
Matt <msalivar@yahoo.com>
2015-10-08 18:48:54 -0700

    Added Unirest

--- app/src/main/java/com/example/cil/freya/MainActivity.java
+++ app/src/main/java/com/example/cil/freya/MainActivity.java
@@ -4,13 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import org.json.JSONObject;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button create, read, update, delete;
+    TextView createText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        create = (Button)findViewById(R.id.create);
+        read = (Button)findViewById(R.id.read);
+        update = (Button)findViewById(R.id.update);
+        delete = (Button)findViewById(R.id.delete);
+        createText = (TextView)findViewById(R.id.editText);
+
+        create.setOnClickListener(this);
+        read.setOnClickListener(this);
+        update.setOnClickListener(this);
+        delete.setOnClickListener(this);
     }
 
     @Override
@@ -33,5 +53,63 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case(R.id.create):
+                createText.setText("create");
+                break;
+            case(R.id.read):
+                createText.setText("read");
+                break;
+            case(R.id.delete):
+                createText.setText("delete");
+                break;
+            case(R.id.update):
+                createText.setText("update");
+                break;
+        }
+    }
+
+    public void sendMessage(View view) {
+
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+
+        TextView txtView = (TextView) findViewById(R.id.textView1);
+        txtView.setText(message);
+
+        new CallMashapeAsync().execute(message);
+    }
+
+    private class CallMashapeAsync extends AsyncTask<String, Integer, HttpResponse<JsonNode>> {
+
+        protected HttpResponse<JsonNode> doInBackground(String... msg) {
+
+            HttpResponse<JsonNode> request = null;
+            try {
+                request = Unirest.get("https://webknox-question-answering.p.mashape.com/questions/answers?question=" + msg[0] + "&answerLookup=true&answerSearch=true")
+                        .header("X-Mashape-Authorization", "<Insert Mashape key here>")
+                        .asJson();
+            } catch (UnirestException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return request;
+        }
+
+        protected void onProgressUpdate(Integer...integers) {
+        }
+
+        protected void onPostExecute(HttpResponse<JsonNode> response) {
+            String answer = response.getBody().toString();
+            TextView txtView = (TextView) findViewById(R.id.textView1);
+            txtView.setText(answer);
+        }
     }
 }
