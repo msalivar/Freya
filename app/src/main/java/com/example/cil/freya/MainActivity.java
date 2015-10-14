1410f5c62ef4e701bec11a29e7356a93af44855e
Matt <msalivar@yahoo.com>
2015-10-13 18:19:51 -0700

    working on URL

--- app/src/main/java/com/example/cil/freya/MainActivity.java
+++ app/src/main/java/com/example/cil/freya/MainActivity.java
@@ -2,30 +2,48 @@ package com.example.cil.freya;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.JsonToken;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.JsonReader;
+import android.util.JsonWriter;
+import java.io.InputStream;
 
 import org.json.JSONObject;
+import org.json.JSONObject;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.net.HttpURLConnection;
+
+
+
+public class MainActivity  extends Activity implements View.OnClickListener {
 
     Button create, read, update, delete;
     TextView createText;
+    JsonReader test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        create = (Button)findViewById(R.id.create);
-        read = (Button)findViewById(R.id.read);
-        update = (Button)findViewById(R.id.update);
-        delete = (Button)findViewById(R.id.delete);
-        createText = (TextView)findViewById(R.id.editText);
+        create = (Button) findViewById(R.id.create);
+        read = (Button) findViewById(R.id.read);
+        update = (Button) findViewById(R.id.update);
+        delete = (Button) findViewById(R.id.delete);
+        createText = (TextView) findViewById(R.id.editText);
 
         create.setOnClickListener(this);
         read.setOnClickListener(this);
@@ -56,60 +74,80 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View v)
-    {
-        switch (v.getId())
-        {
-            case(R.id.create):
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.create):
+                readMessage(test);
                 createText.setText("create");
                 break;
-            case(R.id.read):
+
+            case (R.id.read):
                 createText.setText("read");
                 break;
-            case(R.id.delete):
+            case (R.id.delete):
                 createText.setText("delete");
                 break;
-            case(R.id.update):
+            case (R.id.update):
                 createText.setText("update");
                 break;
         }
     }
 
-    public void sendMessage(View view) {
 
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
 
-        TextView txtView = (TextView) findViewById(R.id.textView1);
-        txtView.setText(message);
 
-        new CallMashapeAsync().execute(message);
+    public boolean readMessage(JsonReader reader)  {
+        String id = null;
+        String text = null;
+        //User user = null;
+        List geo = null;
+        URL url = null;
+
+        try {
+            url = new URL("http://sensor.nevada.edu/GS/Services/people/"); //TODO: set as global var
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+
+
+            int responseCode = urlConnection.getResponseCode();
+            urlConnection.disconnect();
+            return responseCode != 200;
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+
+       /* reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("Creation Date")) {
+                id = reader.nextString();
+            }/* else if (name.equals("text")) {
+                text = reader.nextString();
+            } else if (name.equals("geo") && reader.peek() != JsonToken.NULL) {
+                geo = readDoublesArray(reader);
+            } else if (name.equals("user")) {
+                user = readUser(reader);
+            } else {
+                reader.skipValue();
+            }*/
+       /* }
+        reader.endObject();
+
+        } catch( IOException ex) {
+            System.err.println ("Test");
+        }
+
+        return id;*/
     }
 
-    private class CallMashapeAsync extends AsyncTask<String, Integer, HttpResponse<JsonNode>> {
 
-        protected HttpResponse<JsonNode> doInBackground(String... msg) {
 
-            HttpResponse<JsonNode> request = null;
-            try {
-                request = Unirest.get("https://webknox-question-answering.p.mashape.com/questions/answers?question=" + msg[0] + "&answerLookup=true&answerSearch=true")
-                        .header("X-Mashape-Authorization", "<Insert Mashape key here>")
-                        .asJson();
-            } catch (UnirestException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
 
-            return request;
-        }
 
-        protected void onProgressUpdate(Integer...integers) {
-        }
-
-        protected void onPostExecute(HttpResponse<JsonNode> response) {
-            String answer = response.getBody().toString();
-            TextView txtView = (TextView) findViewById(R.id.textView1);
-            txtView.setText(answer);
-        }
-    }
 }
