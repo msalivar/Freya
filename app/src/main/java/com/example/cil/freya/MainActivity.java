2591aeb72c7162c4c9a80de247101c20bbdc90c4
Matt <msalivar@yahoo.com>
2015-10-14 21:27:54 -0700

    Got http working

--- app/src/main/java/com/example/cil/freya/MainActivity.java
+++ app/src/main/java/com/example/cil/freya/MainActivity.java
@@ -13,19 +13,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.util.JsonReader;
 import android.util.JsonWriter;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.InputStream;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.net.HttpURLConnection;
-
+import java.io.*;
+import java.util.Map;
 
 
 public class MainActivity  extends Activity implements View.OnClickListener {
@@ -76,12 +83,14 @@ public class MainActivity  extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+
             case (R.id.create):
-                readMessage(test);
+                writeMessage();
                 createText.setText("create");
                 break;
 
             case (R.id.read):
+                readMessage();
                 createText.setText("read");
                 break;
             case (R.id.delete):
@@ -93,61 +102,111 @@ public class MainActivity  extends Activity implements View.OnClickListener {
         }
     }
 
-
-
-
-    public boolean readMessage(JsonReader reader)  {
-        String id = null;
-        String text = null;
-        //User user = null;
-        List geo = null;
+    public void writeMessage() {
+        String sb = "";
         URL url = null;
+        int one = 1;
+        HttpURLConnection urlConnection = null;
 
+        String test = "http://sensor.nevada.edu/GS/Services/people/";
         try {
-            url = new URL("http://sensor.nevada.edu/GS/Services/people/"); //TODO: set as global var
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            url = new URL(test);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoOutput(true);
+            urlConnection.setUseCaches(false);
+            urlConnection.setConnectTimeout(10000);
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setRequestProperty("Host", "android.schoolportal.gr");
+            urlConnection.connect();
 
+            //Create JSONObject here
+            JSONObject jsonParam = new JSONObject();
+            jsonParam.put("Creation Date", "Tue, 14 Jul 2015 20:03:03 GMT");
+            jsonParam.put("Email", "test@email.com");
+            jsonParam.put("First Name", "Matt");
+            jsonParam.put("Last Name", "Salivar");
+            jsonParam.put("Modification Date", "Tue, 14 Jul 2015 20:03:03 GMT");
+            jsonParam.put("Organization", "UNR");
+            jsonParam.put("Person", one);
+            jsonParam.put("Phone", "(775)313-7829");
+            jsonParam.put("Photo", null);
+            jsonParam.put("Unique Identifier", "0E984725-C51C-4BF4-9960-E1C80E27ABB7");
 
+            OutputStreamWriter out = new OutputStreamWriter(urlConnection.getOutputStream());
+            out.write(jsonParam.toString());
+            out.close();
 
-            int responseCode = urlConnection.getResponseCode();
-            urlConnection.disconnect();
-            return responseCode != 200;
-        } catch (MalformedURLException e)
-        {
+            int HttpResult = urlConnection.getResponseCode();
+            if(HttpResult == HttpURLConnection.HTTP_OK){
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        urlConnection.getInputStream(),"utf-8"));
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sb += (line + "\n");
+                }
+                br.close();
+
+                System.out.println("" + sb);
+
+            }else{
+                System.out.println(urlConnection.getResponseMessage());
+            }
+        } catch (MalformedURLException e) {
+
             e.printStackTrace();
-            return false;
+
         } catch (IOException e) {
+
             e.printStackTrace();
-            return false;
+
+        } catch (JSONException e) {
+
+            e.printStackTrace();
+
+        } finally{
+
+            if(urlConnection != null)
+                urlConnection.disconnect();
         }
+    }
 
+    public void readMessage()
+    {
+        URL url;
+        HttpURLConnection urlConnection = null;
+        try {
+            url = new URL("http://sensor.nevada.edu/GS/Services/people/");
 
-       /* reader.beginObject();
-        while (reader.hasNext()) {
-            String name = reader.nextName();
-            if (name.equals("Creation Date")) {
-                id = reader.nextString();
-            }/* else if (name.equals("text")) {
-                text = reader.nextString();
-            } else if (name.equals("geo") && reader.peek() != JsonToken.NULL) {
-                geo = readDoublesArray(reader);
-            } else if (name.equals("user")) {
-                user = readUser(reader);
-            } else {
-                reader.skipValue();
-            }*/
-       /* }
-        reader.endObject();
+            urlConnection = (HttpURLConnection) url
+                    .openConnection();
 
-        } catch( IOException ex) {
-            System.err.println ("Test");
+            InputStream in = urlConnection.getInputStream();
+
+            InputStreamReader isw = new InputStreamReader(in);
+
+            int data = isw.read();
+            while (data != -1) {
+                char current = (char) data;
+                data = isw.read();
+                System.out.print(current);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                urlConnection.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace(); //If you want further info on failure...
+            }
         }
+    }
 
-        return id;*/
     }
 
 
 
 
 
-}
+
