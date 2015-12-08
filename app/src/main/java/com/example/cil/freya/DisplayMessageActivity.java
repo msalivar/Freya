9cdb6221cf17b0cae997d02d5e566bb89c3f7a51
hannahmunoz <hannah.munoz92@gmail.com>
2015-12-08 15:58:07 -0800

    Commented

--- app/src/main/java/com/example/cil/freya/DisplayMessageActivity.java
+++ app/src/main/java/com/example/cil/freya/DisplayMessageActivity.java
@@ -7,8 +7,10 @@ import android.widget.TextView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+// used as a template to display JSONS on a new intent. This example shows how to display the people JSON
 public class DisplayMessageActivity extends Activity
 {
+    // create GUI
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -18,12 +20,14 @@ public class DisplayMessageActivity extends Activity
         String[] people = getIntent().getStringArrayExtra(MainActivity.JSON_TEXT);
         String message = "";
 
+        // try to siplay info from the JSON
         try
         {
             for(int j = 0; j < people.length; j++)
             {
+                
                 JSONObject obj = new JSONObject(people[j]);
-
+                // add parts to a JSON to message string
                 message += "Creation Date: " + obj.getString("Creation Date") + "\n";
                 message += "Email: " + obj.getString("Email") + "\n";
                 message += "First Name: " + obj.getString("First Name") + "\n";
@@ -38,10 +42,12 @@ public class DisplayMessageActivity extends Activity
         }
         catch (JSONException e)
         {
+            // print to log cat
             e.printStackTrace();
         }
         // Create the text view
         TextView jsonText = new TextView(this);
+        // put  string in jsontext
         jsonText.setText(message);
 
         // Set the text view as the activity layout
