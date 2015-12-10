b45c3604ee409492cd858ca0e4d287ae74a2778c
samanthagrant <grantsamk@gmail.com>
2015-12-10 10:02:22 -0800

    Commented

--- app/src/main/java/com/example/cil/freya/SearchActivity.java
+++ app/src/main/java/com/example/cil/freya/SearchActivity.java
@@ -7,14 +7,18 @@ import android.widget.Spinner;
 
 public class SearchActivity extends Activity
 {
+    // Declare Variables
     String[] types;
     Spinner typeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Open search activity layout
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        
+        // Populate spinner with different modules
         types = new String[8];
         types[0] = "People";
         types[1] = "Project";
@@ -24,6 +28,8 @@ public class SearchActivity extends Activity
         types[5] = "Component";
         types[6] = "Document";
         types[7] = "Service Entry";
+        
+        // Display populated spinner on layout
         typeSpinner = (Spinner) findViewById(R.id.typeSpinner);
         ArrayAdapter<String> spinAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, types);
         spinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
