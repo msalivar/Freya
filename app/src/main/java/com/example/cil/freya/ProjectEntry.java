5f1439d5d5781f90b02437771132d1de2a84be78
samanthagrant <grantsamk@gmail.com>
2015-12-10 10:07:29 -0800

    Commented

--- app/src/main/java/com/example/cil/freya/ProjectEntry.java
+++ app/src/main/java/com/example/cil/freya/ProjectEntry.java
@@ -2,19 +2,24 @@ package com.example.cil.freya;
 
 public class ProjectEntry
 {
+    // Declare variables
     String name;
     boolean checked;
 
+    // Constructor
     public ProjectEntry(String name, boolean value)
     {
         this.name = name;
         this.checked = value;
     }
 
+    // Get the projected name
     public String getName()
     {
         return this.name;
     }
+    
+    // See if the option is checked on the screen
     public boolean getValue()
     {
         return this.checked;
