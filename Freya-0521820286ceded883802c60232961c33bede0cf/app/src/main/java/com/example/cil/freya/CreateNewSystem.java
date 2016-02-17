24579d85f6ab79b3a1e93bdfe74195693a964012
Matt <msalivar@yahoo.com>
2016-02-16 16:32:26 -0800

    not here

---
+++ Freya-0521820286ceded883802c60232961c33bede0cf/app/src/main/java/com/example/cil/freya/CreateNewSystem.java
@@ -0,0 +1,116 @@
+package com.example.cil.freya;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.UUID;
+
+/**
+ * Created by cil on 11/19/15.
+ */
+public class CreateNewSystem extends Activity implements View.OnClickListener
+{
+    Button createButton, backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_new_systems);
+        createButton = (Button) findViewById(R.id.newSystemButton);
+        createButton.setOnClickListener(this);
+        backButton = (Button) findViewById(R.id.backSystemButton);
+        backButton.setOnClickListener(this);
+
+        /*try {
+            read();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    public void onClick(View v)
+    {
+        Intent intent;
+        switch (v.getId())
+        {
+            case (R.id.newSystemButton):
+                try
+                {newSystem();} catch (JSONException e) {e.printStackTrace();}
+                intent = new Intent(this, CreateNewDeployment.class);
+                 startActivity(intent);
+//                try {
+//                    write();
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+                break;
+
+            case (R.id.backSystemButton):
+                intent = new Intent(this, CreateNewSite.class);
+                startActivity(intent);
+//                try {
+//                    write();
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+                break;
+        }
+    }
+
+    public void newSystem() throws JSONException
+    {
+
+        //Create JSONObject here
+        JSONObject JSON = createSystemJSON();
+
+        JSONArray system = new JSONArray();
+        system.put(JSON);
+
+        CreateNewProject.complete.put("System", system);
+    }
+
+    public JSONObject createSystemJSON() throws JSONException{
+        JSONObject jsonParam = new JSONObject();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.US);
+        String date = sdf.format(new Date());
+        jsonParam.put("Create Date", date);
+        EditText info = (EditText) findViewById(R.id.details);
+        jsonParam.put("Details", info.getText().toString());
+
+        //from gps
+        info = (EditText) findViewById(R.id.location);
+        jsonParam.put("Installation Location",info);
+
+        info = (EditText) findViewById(R.id.name);
+        jsonParam.put("Name", info.getText().toString());
+
+        jsonParam.put("Create Date", date);
+
+        jsonParam.put("Installation Date", date);
+
+
+
+        jsonParam.put("Manager", null);
+        jsonParam.put("Modification Date", date);
+
+        info = (EditText) findViewById(R.id.power);
+        jsonParam.put("Power", info.getText().toString());
+        jsonParam.put("Site", null);
+        jsonParam.put("Unique Identifier", UUID.randomUUID().toString());
+
+        return jsonParam;
+    }
+
+}
---
+++ Freya-0521820286ceded883802c60232961c33bede0cf/app/src/main/java/com/example/cil/freya/ProjectFilterActivity.java
@@ -0,0 +1,85 @@
+package com.example.cil.freya;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ProjectFilterActivity extends Activity implements View.OnClickListener
+{
+    // Declare variables
+    CustomListAdapter adapter = null;
+    Button saveButton;
+
+    // Create project list screen
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        // Display new activity
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_filter);
+        saveButton = (Button)findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(this);
+        for(Boolean entry : MainActivity.projectHideValues)
+        {
+            if (entry) { MainActivity.checkValues.add(true); }
+            else { MainActivity.checkValues.add(false); }
+        }
+        // Options can be checked on the screen
+        adapter = new CustomListAdapter(this, R.layout.row, MainActivity.projectEntries, MainActivity.checkValues);
+        ListView listView = (ListView) findViewById(R.id.projectList);
+        listView.setAdapter(adapter);
+    }
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case (R.id.saveButton):
+                String toWrite = "";
+                for (int i = 0; i < MainActivity.projectEntries.size(); i++)
+                {
+                    toWrite += MainActivity.projectEntries.get(i).getName() + ";" + MainActivity.checkValues.get(i).toString() + ',';
+                }
+                try
+                {
+                    write(toWrite);
+                    List<String> checked = new LinkedList<>();
+                    for (int i = 0; i < MainActivity.projectEntries.size(); i++)
+                    {
+                        if (MainActivity.checkValues.get(i))
+                        {
+                            checked.add(MainActivity.projectEntries.get(i).getName());
+                        }
+                    }
+                    MainActivity.listAdapter = new ArrayAdapter<>(this, R.layout.list_view_layout, checked);
+                    MainActivity.projectList.setAdapter(MainActivity.listAdapter);
+                } catch (FileNotFoundException e)
+                {
+                    e.printStackTrace();
+                }
+                finish();
+        }
+    }
+
+    public void write(String input) throws FileNotFoundException
+    {
+        try {
+            FileOutputStream FileOut = openFileOutput(MainActivity.ProjectFile, MODE_PRIVATE);
+            OutputStreamWriter outputWriter=new OutputStreamWriter(FileOut);
+            outputWriter.write(input);
+            outputWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
